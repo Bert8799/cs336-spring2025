0968bb355a8ef5dabf244d2652ea676f8240f995
pytorch <pytorch@example.com>
2025-12-15 16:18:37 +0800

    feat(hf_train_bpe.py): add huggingface bpe

--- assignment1-basics/run.py
+++ assignment1-basics/run.py
@@ -1,4 +1,5 @@
 from cs336_basics.train_bpe import train_bpe
+from cs336_basics.hf_train_bpe import hf_train_bpe
 
 
 def run_train_bpe(
@@ -6,22 +7,41 @@ def run_train_bpe(
     vocab_size,
     special_tokens,
     merge_outpath=None,
-    vocab_outpath=None
+    vocab_outpath=None,
+    type: str = "custom" or "hf" or None,
 ) -> tuple[dict[int, str], list[tuple[bytes, bytes]]]:
-    return train_bpe(
-        input_path=input_path,
-        vocab_size=vocab_size,
-        special_tokens=special_tokens,
-        merge_outpath=merge_outpath,
-        vocab_outpath=vocab_outpath
-    )
+    if type is None:
+        raise ValueError("Type must be specified as 'custom' or 'hf'.")
+    elif type == "custom":
+        return train_bpe(
+            input_path=input_path,
+            vocab_size=vocab_size,
+            special_tokens=special_tokens,
+            merge_outpath=merge_outpath,
+            vocab_outpath=vocab_outpath
+        )
+    elif type == "hf":
+        return hf_train_bpe(
+            input_path=input_path,
+            vocab_size=vocab_size,
+            special_tokens=special_tokens,
+            merge_outpath=merge_outpath,
+            vocab_outpath=vocab_outpath
+        )
 
 
 if __name__ == "__main__":
+    run_train_bpe_dataset = "openwebtext"
+
+    run_train_bpe_type = "hf"
+    run_train_bpe_path = f"{run_train_bpe_type}_" if run_train_bpe_type != "custom" else ""
+
     run_train_bpe(
-        input_path='data/TinyStoriesV2-GPT4-valid.txt',
-        vocab_size=10_000,
-        special_tokens=["<|endoftext|>","<|endoftext|><|endoftext|>"],
-        merge_outpath='result/TinyStories-valid_bpe_merges.yaml',
-        vocab_outpath='result/TinyStories-valid_bpe_vocab.yaml'
+        input_path="data/owt_train.txt",
+        vocab_size=32_000,
+        special_tokens=["<|endoftext|>", "<|endoftext|><|endoftext|>"],
+        merge_outpath=f"result/tokenizer/{run_train_bpe_dataset}/{run_train_bpe_path}train_merges.yaml",
+        vocab_outpath=f"result/tokenizer/{run_train_bpe_dataset}/{run_train_bpe_path}train_vocab.yaml",
+        type=run_train_bpe_type
     )
+
